cd7b9e4beba3ca79f5d322f833039c04f7dacc96
Ziwei Pan <44417006+PanZiwei@users.noreply.github.com>
2020-04-01 20:16:58 -0400

    Change directory location
    Change $pwd into ..

--- process_genome.sh
+++ process_genome.sh
@@ -1,29 +1,30 @@
+PWD=/projects/li-lab/Ziwei/DeepHM/forCompress
 ##process genomic features like gc percent, cpg num, dist to cpg island
 mkdir $PWD/$2
-cd $PWD/$2
+cd $PWD/$2 ##This step will direct to the $pwd/$2 and use it as the working directory later
 #gc percent
-python3 $PWD/src/extract_feature_from_bp3.py $PWD/$1/gc5Base.sort.bedGraph $PWD/$1/cpg_no_chrM $PWD/$1/mm9_chrom_sizes cortex_gcContent_window2
+python3 ../src/extract_feature_from_bp3.py $PWD/$1/gc5Base.sort.bedGraph ../$1/cpg_no_chrM $PWD/$1/mm9_chrom_sizes cortex_gcContent_window2
 #remove first 5 column (change)
-cut -f5- cortex_gcContent_window2 cortex_gcContent_window2_cut
+cut -f5- cortex_gcContent_window2 >cortex_gcContent_window2_cut
 
 #cpg num at each window
-python3 $PWD/src/generate_cpg_window.py $PWD/$1/cpg_no_chrM cpg_window
-bedtools coverage -a cpg_window -b $PWD/$1/cpg_no_chrM  -counts >cpg_num_window
+python3 ../src/generate_cpg_window.py ../$1/cpg_no_chrM cpg_window
+bedtools coverage -a cpg_window -b ../$1/cpg_no_chrM  -counts >cpg_num_window
 cut -f4 cpg_num_window >cpg_num_window_cut
-Rscript $PWD/src/format_cpgNum_data.R cpg_num_window_cut cpg_num_window_final
+Rscript ../src/format_cpgNum_data.R cpg_num_window_cut cpg_num_window_final
 
 #dist to cpg island
-bedtools intersect -a $PWD/$1/cpg_no_chrM -b $PWD/$1/cpgIsland.bed -wao -sorted >cpg_inter_cgi
+bedtools intersect -a ../$1/cpg_no_chrM -b ../$1/cpgIsland.bed -wao -sorted >cpg_inter_cgi
 cut -f1-3,7 cpg_inter_cgi >cpg_inter_cgi_cut
-python3 $PWD/src/cal_dist_to_cgi.py cpg_inter_cgi_cut cpg_dist_cgi
+python3 ../src/cal_dist_to_cgi.py cpg_inter_cgi_cut cpg_dist_cgi
 
 #genome encoding (add)
-python3 -u $PWD/src/create_genome_encoding2.py $PWD/$1/mm9.fa mm9_genome_encoding
+python3 -u ../src/create_genome_encoding2.py ../$1/mm9.fa mm9_genome_encoding
 
 #mre site (add)
-cat $PWD/$1/ACGT_sites.bed $PWD/$1/GCGC_sites.bed  $PWD/$1/CCGC_sites.bed $PWD/$1/CCGG_sites.bed >four_site.bed
+cat ../$1/ACGT_sites.bed ../$1/GCGC_sites.bed  ../$1/CCGC_sites.bed ../$1/CCGG_sites.bed >four_site.bed
 sort -k1,1 -k2,2n four_site.bed >mre_four_site_sort.bed
-bedtools intersect -a $PWD/$1/cpg_no_chrM -b   mre_four_site_sort.bed -u -sorted >data_inter_mreSite
+bedtools intersect -a ../$1/cpg_no_chrM -b   mre_four_site_sort.bed -u -sorted >data_inter_mreSite
 sed "s/$/\t1/" data_inter_mreSite >data_inter_mreSite_add1
 bedtools intersect -a $PWD/$1/cpg_no_chrM -b mre_four_site_sort.bed -v -sorted >data_notInter_mreSite
 sed "s/$/\t0/" data_notInter_mreSite >data_notInter_mreSite_add0
